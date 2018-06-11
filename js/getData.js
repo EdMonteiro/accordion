5950d06ac2fae7ed3dcfae5dcc9787eb900f1af0
Edemir Monteiro <edemirmonteiro@gmail.com>
2018-06-11 08:00:52 +0100

    Add toggle functionality - address for loop later

--- js/getData.js
+++ js/getData.js
@@ -13,24 +13,30 @@ ourRequest.onload = function() {
  `;
   // renderHtml(ourData); 
 
+
+/* Get a forEach loop going on after prototyping */
   document.getElementById("btn1").addEventListener("click", function() {
-    
-    document.getElementById("paragraph1").classList.toggle("mystyle");
-    if( ('.mystyle'==='active') ) {
-      document.getElementById("paragraph1").innerHTML = `<p>${ourData.blocks["0"].content}</p>`;
-    }
-    
+    if( (document.getElementById('paragraph1').classList.toggle("mystyle")) ) {
+        document.getElementById("paragraph1").innerHTML = `<p>${ourData.blocks["0"].content}</p>`;
+      } else {
+        document.getElementById("paragraph1").innerHTML = `<p id="paragraph1"></p>`;;
+      }
   })
-  // document.getElementById("btn2").addEventListener("click", function() {
-  //   document.getElementById("paragraph2").innerHTML = `<p>${ourData.blocks["1"].content}</p>`;
-  // })
-  // document.getElementById("btn3").addEventListener("click", function() {
-  //   document.getElementById("paragraph3").innerHTML = `<p>${ourData.blocks["2"].content}</p>`;
-  // });
 
-  
- 
-
+  document.getElementById("btn2").addEventListener("click", function() {
+    if( (document.getElementById('paragraph2').classList.toggle("mystyle")) ) {
+        document.getElementById("paragraph2").innerHTML = `<p>${ourData.blocks["1"].content}</p>`;
+      } else {
+        document.getElementById("paragraph2").innerHTML = `<p id="paragraph1"></p>`;;
+      }
+  })
+  document.getElementById("btn3").addEventListener("click", function() {
+    if( (document.getElementById('paragraph3').classList.toggle("mystyle")) ) {
+        document.getElementById("paragraph3").innerHTML = `<p>${ourData.blocks["2"].content}</p>`;
+      } else {
+        document.getElementById("paragraph3").innerHTML = `<p id="paragraph1"></p>`;;
+      }
+  })
 };
 
 ourRequest.send();
