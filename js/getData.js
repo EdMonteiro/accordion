d00becfb329b12728201c2d74f419e3c069612c1
Edemir Monteiro <edemirmonteiro@gmail.com>
2018-06-11 09:55:57 +0100

    Add arrow to header with toggle efect

--- js/getData.js
+++ js/getData.js
@@ -6,7 +6,7 @@ ourRequest.onload = function() {
   document.getElementById("app").innerHTML = `
     <h1 class="header" id="btn1">
       ${ourData.blocks["0"].heading}
-      <p id="arrow">&#709;</p>
+      <p id="arrow1">&#709;</p>
     </h1>
     <p id="paragraph1"></p>
 
@@ -26,35 +26,37 @@ ourRequest.onload = function() {
 
 
 /* Get a forEach loop going on after prototyping */
+/* Get a more similar arrow icon or make with css*/
   document.getElementById("btn1").addEventListener("click", function() {
     if( (document.getElementById('paragraph1').classList.toggle("mystyle")) ) {
         document.getElementById("paragraph1").innerHTML = `<p>${ourData.blocks["0"].content}</p>`;
-        document.getElementById("arrow").innerHTML = `<p id="arrow">&#708;</p>`;
+        document.getElementById("arrow1").innerHTML = `<p id="arrow1">&#708;</p>`;
       } else {
         document.getElementById("paragraph1").innerHTML = `<p id="paragraph1"></p>`;
-        document.getElementById("arrow").innerHTML = `<p id = "arrow">&#709;</p>`;
+        document.getElementById("arrow1").innerHTML = `<p id = "arrow1">&#709;</p>`;
       }
   })
 
-  // document.getElementById("btn2").addEventListener("click", function() {
-  //   if( (document.getElementById('paragraph2').classList.toggle("mystyle")) ) {
-  //       document.getElementById("paragraph2").innerHTML = `<p>${ourData.blocks["1"].content}</p>`;
-  //       document.getElementsByClassName("arrow").innerHTML = `<p class = "arrow">&#708;</p>`;
-  //     } else {
-  //       document.getElementById("paragraph2").innerHTML = `<p id="paragraph1"></p>`;
-  //       document.getElementsByClassName("arrow").innerHTML = `<p class = "arrow">&#709;</p>`;
-  //     }
-  // })
-  // document.getElementById("btn3").addEventListener("click", function() {
-  //   if( (document.getElementById('paragraph3').classList.toggle("mystyle")) ) {
-  //       document.getElementById("paragraph3").innerHTML = `<p>${ourData.blocks["2"].content}</p>`;
-  //       // document.getElementById("arrow").innerHTML = `<p id = "arrow">&#708;</p>`;
-  //     } else {
-  //       document.getElementById("paragraph3").innerHTML = `<p id="paragraph1"></p>`;
-  //       // document.getElementById("arrow").innerHTML = `<p id = "arrow">&#709;</p>`;
-
-  //     }
-  // })
+  document.getElementById("btn2").addEventListener("click", function() {
+    if( (document.getElementById('paragraph2').classList.toggle("mystyle")) ) {
+        document.getElementById("paragraph2").innerHTML = `<p>${ourData.blocks["1"].content}</p>`;
+        document.getElementById("arrow2").innerHTML = `<p id="arrow2">&#708;</p>`;
+      } else {
+        document.getElementById("paragraph2").innerHTML = `<p id="paragraph2"></p>`;
+        document.getElementById("arrow2").innerHTML = `<p id = "arrow2">&#709;</p>`;
+      }
+  })
+  document.getElementById("btn3").addEventListener("click", function() {
+    if( (document.getElementById('paragraph3').classList.toggle("mystyle")) ) {
+        document.getElementById("paragraph3").innerHTML = `<p>${ourData.blocks["2"].content}</p>`;
+        document.getElementById("arrow3").innerHTML = `<p id="arrow3">&#708;</p>`;
+      } else {
+        document.getElementById("paragraph3").innerHTML = `<p id="paragraph3"></p>`;
+        document.getElementById("arrow3").innerHTML = `<p id = "arrow3">&#709;</p>`;
+      }
+  })
 };
+
+
 
 ourRequest.send();
